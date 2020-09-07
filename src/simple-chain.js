cd8fa8151f30d2b703e163295fe5406a9cbf54ff
VBiralo <mr.biralo@yandex.ru>
2020-09-08 00:13:11 +0300

    feat: add simpleChain functional

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,29 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain[position]) throw new Error();
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+
+    this.chain.map(link => {
+      str += '( ' + link + ' )';
+    })
+
+    return str.replace(/\)\(/g, ')~~(', );
   }
 };
 
