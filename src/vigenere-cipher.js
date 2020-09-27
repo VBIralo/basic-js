2f617dd533c2c1fd09471c7aef102804047bb3c6
VBiralo <mr.biralo@yandex.ru>
2020-09-27 19:57:54 +0300

    feat: add vigenereCipher functional

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.direct = true;
+  };
+
+  encrypt(message, key) {
+    if (arguments.length < 2) throw new Error('Error!');
+
+    let messUp = message.toUpperCase().split('');
+    let keyUp = key.toUpperCase().split('');
+    let result = '';
+
+    let i = 0;
+
+    messUp.map(char => {
+      let chCode = char.charCodeAt(0);
+      if (chCode > 64 && chCode < 91) {
+        let keyCode = keyUp[i++].charCodeAt(0);
+        if (keyCode > 64 && keyCode < 91) {
+          result += String.fromCharCode(65 + ((chCode + keyCode) % 26));
+        }
+        if (i >= key.length) {
+          i = 0;
+        }
+      } else {
+        result += char;
+      }
+    })
+
+    return (this.direct) ? result : result.split('').reverse();
+  }
+  decrypt(message, key) {
+    if (arguments.length < 2) throw new Error('Error!');
+
+    let messUp = message.toUpperCase().split('');
+    let keyUp = key.toUpperCase().split('');
+    let result = '';
+
+    let i = 0;
+
+    messUp.map(char => {
+      let chCode = char.charCodeAt(0);
+      if (chCode > 64 && chCode < 91) {
+        let keyCode = keyUp[i++].charCodeAt(0);
+        if (keyCode > 64 && keyCode < 91) {
+          result += String.fromCharCode(65 + ((chCode - keyCode + 26) % 26));
+        }
+        if (i >= key.length) {
+          i = 0;
+        }
+      } else {
+        result += char;
+      }
+    })
+
+    return (this.direct) ? result : result.split('').reverse();
   }
 }
 
